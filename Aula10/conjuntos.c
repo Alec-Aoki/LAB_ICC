02e35e2e20b0725f33d09b946f478faa519f6d23
Alec Aoki <alec.camposaoki@gmail.com>
2024-05-21 16:08:23 -0300

    Planning 2

--- Aula10/conjuntos.c
+++ Aula10/conjuntos.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Implemente um programa em C que leia 2 vetores (lendo o tamanho do vetor antes), os veto-
+res s˜ao compostos por n´umeros inteiros(positivos e negativos) n˜ao ordenados, os vetores devem ser
+compreendidos como conjuntos A e B.
+O programa DEVE conter uma fun¸c˜ao que retorna o conjunto A uni˜ao B e outra que retorne
+o conjunto (A uni˜ao B) - (A intersec¸c˜ao B), os vetores retornados devem ocupar somente o espa¸co
+estritamente necess´ario. Em seguida, o programa deve imprimir os conjuntos como mostrado no
+exemplo abaixo
+
+OBRIGAT ´ORIO que as duas fun¸c˜oes especificadas retornem vetores, e que esses vetores
+ocupem somente o espa¸co utilizado.
+• Os elementos dos conjuntos impressos devem estar ordenados.
+• caso o conjunto seja vazio, deve ser impresso ”vazio”.
+
+main:
+-> ler tamanho dos vetores
+-> receber comeco dos vetores em dois ponteiros diferentes
+-> criar vetor pra uniao e interseccao
+
+Leia_Elementos:
+-> calloc
+-> retorna ponteiro para o inicio desse vetor
+
+Ordene_Elementos:
+-> bubble sort neles
+-> usar indicies
+-> return nada
+
+Uniao_AB:
+-> jogar A no vetor Uniao
+-> verificar se o elemento de B ja existe em Uniao
+    -> se sim: fodase
+    -> se nao: colocar no vetor Uniao
+    -> Ordene_Elementos vetor Uniao
+
+Intersec_AB:
+-> jogar A no vetor Intersec
+-> verificar se o elemento de B ja existe em Intersec
+    -> se sim: tirar do Intersec
+    -> se nao: colocar no vetor Intersec
+    -> Ordene_Elementos vetor Intersec
+*/
+
 int main(void){
 
     return 0;
